35a3b9f72ddaa4ddb2b8d1ee623f4a511c36c085
Joel Howard <subsidel@users.noreply.github.com>
2015-06-12 13:46:18 +0100

    swap grunt-jasmine-node with grunt jasmine-nodejs
    So we can have pretty colours and use jasmine v2.x

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,16 +30,41 @@ module.exports = function(grunt) {
         src: ['lib/**/*.js', 'test/**/*.js']
       }
     },
-    jasmine_node: {
-      options: {
-        forceExit: true,
-        match: '.',
-        matchall: false,
-        extensions: 'js',
-        specNameMatcher: 'spec'
-      },
-      all: []
-    },
+    jasmine_nodejs: {
+            // task specific (default) options
+            options: {
+                specNameSuffix: "Spec.js", // also accepts an array
+                helperNameSuffix: "helper.js",
+                useHelpers: false,
+                stopOnFailure: false,
+                // configure one or more built-in reporters
+                reporters: {
+                    console: {
+                        colors: true,
+                        cleanStack: 1,       // (0|false)|(1|true)|2|3
+                        verbosity: 3,        // (0|false)|1|2|(3|true)
+                        listStyle: "indent", // "flat"|"indent"
+                        activity: false
+                    }
+                },
+                // add custom Jasmine reporter(s)
+                customReporters: []
+            },
+            your_target: {
+                // target specific options
+                options: {
+                    useHelpers: true
+                },
+                // spec files
+                specs: [
+                    "spec/**",
+                    "test/core/**"
+                ],
+                helpers: [
+                    "test/helpers/**"
+                ]
+            }
+        },
     watch: {
       gruntfile: {
         files: '<%= jshint.gruntfile.src %>',
